31d99f43facf6c9fdf937aa33cca6e643d184cb3
AndyG4 <104481940+AndyG4@users.noreply.github.com>
2023-06-13 09:26:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+//script.js
 document.getElementById("form-publicacion").addEventListener("submit", function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-  // Obtener los valores de los campos del formulario
-  var titulo = document.getElementById("titulo").value;
-  var contenido = document.getElementById("contenido").value;
-  var etiquetas = document.getElementById("etiquetas").value;
-  var archivo = document.getElementById("archivo").files[0];
+// Obtener los valores de los campos del formulario
+var titulo = document.getElementById("titulo").value;
+var contenido = document.getElementById("contenido").value;
+var etiquetas = document.getElementById("etiquetas").value;
+var archivo = document.getElementById("archivo").files[0];
 
-  // Crear un objeto FormData para enviar
+// Crear un objeto FormData para enviar los datos al servidor
+var formData = new FormData();
+formData.append("titulo", titulo);
+formData.append("contenido", contenido);
+formData.append("etiquetas", etiquetas);
+formData.append("archivo", archivo);
+
+// Enviar los datos al servidor usando AJAX o Fetch
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "publicar.php", true);
+xhr.onreadystatechange = function() {
+if (xhr.readyState === 4 && xhr.status === 200) {
+// Procesar la respuesta del servidor si es necesario
+console.log(xhr.responseText);
+}
+};
+xhr.send(formData);
+});
